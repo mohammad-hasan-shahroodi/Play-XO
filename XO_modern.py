54798233025f06db0e4d92fcf8398bfe72d97f52
mohammad-hasan-shahroodi <140893151+mohammad-hasan-shahroodi@users.noreply.github.com>
2023-08-07 21:25:11 +0330

    new change - for(9) - without continue

--- XO_modern.py
+++ XO_modern.py
@@ -8,9 +8,11 @@ print("\n")
 input("Press enter to continue.")
 print("\n")
 def error1():
-    print("======= you shoud choose number between 1-9 , Your turn was lost for this carelessness =======")
+    print("you should choose a number between 1-9, try agian:")
 def error2():
-    print("======= you shoud choose number , Your turn was lost for this carelessness =======")
+    print("⓿_⓿")
+    print("Don't cheat in the game , try agian: ")
+    print("⓿_⓿")
 def init_table(A):
     home = 1
     for i in range(3):
@@ -30,11 +32,8 @@ def put_number(T,R):
     row = T // 3
     column = T % 3   
     if A[row][column] == "X" or A[row][column] == "O":
-        print("⓿_⓿")
-        print("Don't cheat in the game , Your turn was lost for this carelessness")
-        print("⓿_⓿")
         return False
-    A[row][column] = R
+#    A[row][column] = R
 def Chek_win(R):
     for i in range(3):
         if A[i][0] == R and A[i][1] == R and A[i][2] == R:
@@ -46,50 +45,82 @@ def Chek_win(R):
     if A[0][2] == R and A[1][1] == R and A[2][0] == R:
         return True
     return False
+def check_int(x):
+    try:
+        x = int(x)
+    except:
+        return False
+def check_1_9(x):
+    try:
+        x = int(x)
+    except:
+
+        return False
+    if not 1<=x<=9 :
+        return False    
 init_table(A)
-for move_number in range(10000000000000000000000000000):
+for move_number in range(9):
     if move_number % 2 == 0:
         print("=========== Player 1 turn =============") 
         print_table(A)
         x = input("choose a number: ")
-        try:
-            x = int(x)
-        except:
-            print()
-            error2()
-            print()
-            continue       
-        if 1<=x<=9 :
-            put_number(x-1,"X")
-            if Chek_win("X"):
-                print_table(A)
-                print("=========== Player 1 is won! =============")
-                break           
-        else:
-            print()
+        while check_int(x) == False:
             error1()
-            print()
-            continue          
+            x = input("choose a number: ")
+            check_int(x) 
+        x = int(x)
+        while check_1_9(x) == False:
+            error1()
+            x = input("choose a number: ")
+            check_1_9(x)
+        x = int(x)
+        while put_number(x-1,"X") == False:
+            error2()
+            x = input("choose a number: ")
+            while check_1_9(x) == False:
+                error1()
+                x = input("choose a number: ")
+                check_1_9(x)
+            x = int(x)
+            put_number(x-1,"X")
+        row = (x-1) // 3
+        column = (x-1) % 3
+        A[row][column] = "X"
+        put_number(x-1,"X")
+        if Chek_win("X"):
+            print_table(A)
+            print("=========== Player 1 is won! =============")
+            exit()                    
+         
     else:
         print("=========== Player 2 turn =============") 
         print_table(A)
         x = input("choose a number: ")
-        try:
-            x = int(x)
-        except:
-            print()
-            error2()
-            print()
-            continue       
-        if 1<=x<=9 :
-            put_number(x-1,"O")
-            if Chek_win("O"):
-                print_table(A)
-                print("=========== Player 1 is won! =============")
-                break           
-        else:
-            print()
+        while check_int(x) == False:
             error1()
-            print()
-            continue     
+            x = input("choose a number: ")
+            check_int(x) 
+        x = int(x)
+        while check_1_9(x) == False:
+            error1()
+            x = input("choose a number: ")
+            check_1_9(x)
+        x = int(x)
+        while put_number(x-1,"O") == False:
+            error2()
+            x = input("choose a number: ")
+            while check_1_9(x) == False:
+                error1()
+                x = input("choose a number: ")
+                check_1_9(x)
+            x = int(x)
+            put_number(x-1,"O")
+        row = (x-1) // 3
+        column = (x-1) % 3
+        A[row][column] = "O"
+        put_number(x-1,"O")
+        if Chek_win("O"):
+            print_table(A)
+            print("=========== Player 2 is won! =============")
+            exit()  
 print("table is full")
